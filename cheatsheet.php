333576d49bf1b8720fc1428a6381dca3b769d2f8
saasey <48605836+saasey@users.noreply.github.com>
2020-11-22 15:15:37 -0500

    Update cheatsheet.php

---
+++ cheatsheet.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * 
+ * Cheat sheet
+ * -----------------------
+ * Trig Functions
+ * 000000 = Hyperbolic Cosine
+ * 000001 = Cosine
+ * 000010 = Hyperbolic Sine
+ * 000011 = Sine
+ * 000100 = Hyperbolic Tangent
+ * 000101 = Tangent
+ * 000110 = 1 / Sine(x)
+ * 000111 = 1 / Cosine(x)
+ * 001000 = 1 / Tangent(x)
+ * 001001 = ArcSine(x)
+ * 001010 = ArcCosine(x)
+ * 001011 = ArcTangent(x)
+ * 001100 = Inverse Sine
+ * 001101 = Inverse Cosine
+ * 001110 = Inverse Tangent
+ * ----------------------
+ * Derivative Functions
+ * 001111 = Constant Rule
+ * 010000 = Sum Rule
+ * 010001 = Difference Rule
+ * 010010 = Power Rule (x,y)
+ * 010011 = Product Rule
+ * 010100 = Quotient Rule
+ * 010101 = Chain Rule
+ * ----------------------
+ * Arithmetic
+ * 010110 = Exponent (x,y)
+ * 010111 = Addition Sign
+ * 011000 = Subtraction Sign
+ * 011001 = Multiplication Sign
+ * 011010 = Division Sign
+ * ----------------------
+ * Condition Signs (x,y)
+ * 011011 = Greater Than
+ * 011100 = Less Than
+ * 011101 = Greater Than or Eq
+ * 011110 = Less Than or Eq
+ * 011111 = Not equal
+ * 100000 = Equal
+ * ----------------------
+ * Condition with previous condition (x,y)
+ * .. AND type of banding
+ * 100001 = Equals
+ * 100010 = Not Equal
+ * 100011 = Greater Than
+ * 100100 = Less Than
+ * 100101 = Greater Than or Eq
+ * 100110 = Less Than or Eq
+ * .. OR type of banding
+ * 100111 = Equals
+ * 101000 = Not Equal
+ * 101001 = Greater Than
+ * 101010 = Less Than
+ * 101011 = Greater Than or Eq
+ * 101100 = Less Than or Eq
+ * .. XOR type of banding
+ * 101110 = Equals
+ * 101111 = Not Equal
+ * 110000 = Greater Than
+ * 110001 = Less Than
+ * 110010 = Greater Than or Eq
+ * 110011 = Less Than or Eq
+ * ----------------------
+ * Other maths
+ * 110100 = Factorials
+ * 110101 = e^x
+ * 110110 = ln(x)
+ * 110111 = log_y(x) (x,y)
+ * 
+ */
+
+?>
